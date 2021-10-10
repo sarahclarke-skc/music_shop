4990b1d7edde7ba17661979a3ded443d521efea2
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-10-10 16:37:11 +0100

    Clarinet tests passing

--- src/main/java/instruments/Clarinet.java
+++ src/main/java/instruments/Clarinet.java
@@ -1,4 +1,32 @@
 package instruments;
 
-public class Clarinet {
+import behaviours.ICanPlay;
+import behaviours.ICanSell;
+
+public class Clarinet extends Instrument implements ICanPlay, ICanSell {
+
+    private String clarinetType;
+
+    public Clarinet(double wholeSalePrice, double salePrice, String category, String brand, String model, String clarinetType) {
+        super(wholeSalePrice, salePrice, category, brand, model);
+        this.clarinetType = clarinetType;
+    }
+
+    public String getClarinetType() {
+        return clarinetType;
+    }
+
+    public void setClarinetType(String clarinetType) {
+        this.clarinetType = clarinetType;
+    }
+
+    @Override
+    public String play() {
+        return "I'm a clarinet";
+    }
+
+    @Override
+    public double calculateMarkUp() {
+        return this.getSalePrice() - this.getWholeSalePrice();
+    }
 }
--- src/test/java/instrumentsTests/ClarinetTest.java
+++ src/test/java/instrumentsTests/ClarinetTest.java
@@ -1,4 +1,38 @@
 package instrumentsTests;
 
+import instruments.Clarinet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ClarinetTest {
+
+    Clarinet clarinet;
+
+    @Before
+    public void before() {
+        clarinet = new Clarinet(3000.00, 3600.00, "woodwind", "Buffet", "Vintage", "B flat");
+    }
+
+    @Test
+    public void hasClarinetType() {
+        assertEquals("B flat", clarinet.getClarinetType());
+    }
+
+    @Test
+    public void canChangeClarinetType() {
+        clarinet.setClarinetType("E flat");
+        assertEquals("E flat", clarinet.getClarinetType());
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("I'm a clarinet", clarinet.play());
+    }
+
+    @Test
+    public void canCalculateMarkUp() {
+        assertEquals(600.00, clarinet.calculateMarkUp(), 0.001);
+    }
 }
