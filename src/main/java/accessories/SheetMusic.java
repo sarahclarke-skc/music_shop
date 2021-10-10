546e9a415bc45322b5c2863e8d41323617de2dd4
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-10-10 16:50:12 +0100

    sheet music tests passing

--- src/main/java/accessories/SheetMusic.java
+++ src/main/java/accessories/SheetMusic.java
@@ -1,4 +1,55 @@
 package accessories;
 
-public class SheetMusic {
+import behaviours.ICanSell;
+
+public class SheetMusic implements ICanSell {
+
+    private String name;
+    private String composer;
+    private double wholesalePrice;
+    private double salePrice;
+
+    public SheetMusic(String name, String composer, double wholesalePrice, double salePrice) {
+        this.name = name;
+        this.composer = composer;
+        this.wholesalePrice = wholesalePrice;
+        this.salePrice = salePrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComposer() {
+        return composer;
+    }
+
+    public void setComposer(String composer) {
+        this.composer = composer;
+    }
+
+    public double getWholesalePrice() {
+        return wholesalePrice;
+    }
+
+    public void setWholesalePrice(double wholesalePrice) {
+        this.wholesalePrice = wholesalePrice;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    @Override
+    public double calculateMarkUp() {
+        return this.salePrice - this.wholesalePrice;
+    }
 }
--- src/test/java/accessoriesTests/SheetMusicTest.java
+++ src/test/java/accessoriesTests/SheetMusicTest.java
@@ -1,4 +1,44 @@
 package accessoriesTests;
 
+import accessories.SheetMusic;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SheetMusicTest {
+
+    SheetMusic sheetMusic;
+
+    @Before
+    public void before() {
+        sheetMusic = new SheetMusic("Piano Sonata No. 2 in B-Flat Minor", "Sergei Rachmaninoff", 3.00, 10.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Piano Sonata No. 2 in B-Flat Minor", sheetMusic.getName());
+    }
+
+    @Test
+    public void canChangeName() {
+        sheetMusic.setName("Moonlight Sonata");
+        assertEquals("Moonlight Sonata", sheetMusic.getName());
+    }
+
+    @Test
+    public void hasComposer() {
+        assertEquals("Sergei Rachmaninoff", sheetMusic.getComposer());
+    }
+
+    @Test
+    public void canChangeComposer() {
+        sheetMusic.setComposer("Ludwig Beethoven");
+        assertEquals("Ludwig Beethoven", sheetMusic.getComposer());
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(7.00, sheetMusic.calculateMarkUp(), 0.001);
+    }
 }
