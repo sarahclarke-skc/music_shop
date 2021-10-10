30a62bbfe5915ae91a8e619adac0299ca32b0385
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-10-10 16:27:33 +0100

    Piano tests passing

--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,4 +1,21 @@
 package instruments;
 
-public class Piano {
+import behaviours.ICanPlay;
+import behaviours.ICanSell;
+
+public class Piano  extends Instrument implements ICanSell, ICanPlay {
+
+    public Piano(double wholeSalePrice, double salePrice, String category, String brand, String model) {
+        super(wholeSalePrice, salePrice, category, brand, model);
+    }
+
+    @Override
+    public String play() {
+        return "I'm a piano";
+    }
+
+    @Override
+    public double calculateMarkUp() {
+        return this.getSalePrice() - this.getWholeSalePrice();
+    }
 }
--- src/test/java/instrumentsTests/PianoTest.java
+++ src/test/java/instrumentsTests/PianoTest.java
@@ -1,4 +1,28 @@
 package instrumentsTests;
 
+import instruments.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+
+    Piano piano;
+
+    @Before
+    public void before() {
+        piano = new Piano(10000.00, 16000.00, "percussion", "Steinway & Sons", "K-52");
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("I'm a piano", piano.play());
+    }
+
+    @Test
+    public void canCalculateMarkUp() {
+        assertEquals(6000.00, piano.calculateMarkUp(), 0.001);
+    }
 }
+
